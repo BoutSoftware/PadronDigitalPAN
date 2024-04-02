3a5ede670cfc44ad81ecbe3197828445d94f8918
Costas Rueda Juan Pablo <jcostas29@alumnos.uaq.mx>
2024-04-02 15:11:54 -0600

    Add input validation for changePassword route

--- src/app/dashboard/api/users/[id]/changePassword/route.ts
+++ src/app/dashboard/api/users/[id]/changePassword/route.ts
@@ -1,9 +1,18 @@
 import prisma from "@/configs/database";
+import { hasIncompleteFields } from "@/utils";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
   try {
-    console.log();
+    interface ReqBody { id: string, password: string; }
+
+    const { id, password } = await request.json() as ReqBody;
+
+    if (hasIncompleteFields({ id, password })) {
+      return NextResponse.json({ code: "INCOMPLETE_FIELDS", message: "Password is missing" });
+    }
+
+
     
   } catch (error) {
     console.log(error);
