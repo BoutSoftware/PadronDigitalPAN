6fdd908bac07b19a17aa71201ec24be9906999c6
Costas Rueda Juan Pablo <jcostas29@alumnos.uaq.mx>
2024-04-02 16:03:57 -0600

    Update changePassword API to hash password and handle user not found

--- src/app/dashboard/api/users/[id]/changePassword/route.ts
+++ src/app/dashboard/api/users/[id]/changePassword/route.ts
@@ -1,18 +1,34 @@
 import prisma from "@/configs/database";
 import { hasIncompleteFields } from "@/utils";
 import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcrypt";
 
 export async function POST(request: NextRequest) {
   try {
-    interface ReqBody { id: string, password: string; }
+    interface ReqBody { username: string, password: string; }
 
-    const { id, password } = await request.json() as ReqBody;
+    const { username, password } = await request.json() as ReqBody;
 
-    if (hasIncompleteFields({ id, password })) {
+    if (hasIncompleteFields({ username, password })) {
       return NextResponse.json({ code: "INCOMPLETE_FIELDS", message: "Password is missing" });
     }
 
+    const userExists = await prisma.user.findUnique({ where: { username: username } });
+    if (!userExists) {
+      return NextResponse.json({ code: "NOT_FOUND", message: "User doesn't exists" });
+    }
 
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await prisma.user.update({
+      where: {
+        username: username,
+      },
+      data: {
+        password: hashedPassword
+      }
+    });
+
+    return NextResponse.json({ code: "OK", message: "Password updated succesfully", data: user});
     
   } catch (error) {
     console.log(error);
