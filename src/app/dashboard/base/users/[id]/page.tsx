2ecd1dbd22d5346f6e9a23d1982ba57ebf5f575b
JaviZamudio <javier.elihu100@gmail.com>
2024-04-03 16:58:48 -0600

    fixes and all

--- src/app/dashboard/base/users/[id]/page.tsx
+++ src/app/dashboard/base/users/[id]/page.tsx
@@ -1,28 +1,30 @@
-'use client'
+"use client";
 
 import React from "react";
 import Header from "@/components/Header";
-import { Button } from "@nextui-org/react";
+import { Button, Divider } from "@nextui-org/react";
 import Roles from "@/components/Roles";
 
 export default function IndividualUserPage() {
   const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   return (
-    <div>
-      <Header title="Nombre de Usuario" />
-      <div id="main" className="w-3/4 h-full m-8">
-        <div id="userInfo" className="flex flex-row mt-8 gap-8">
+    <div className="flex flex-grow flex-col p-8">
+      <Header title="[Nombre de Persona]" />
+
+      <main id="main" className="mt-4">
+        <div id="userInfoContainer" className="flex flex-row mt-8 gap-8">
           <div id="image" className="w-48 h-48 bg-blue-200"></div>
-          <div className="flex flex-row gap-x-8">
+          <div id="userInfo" className="flex flex-row gap-x-8">
             <div className="flex flex-col gap-4">
               <p>Nombre: Nombre de la persona</p>
               <p>Número telefónico: 000 000 0000</p>
               <p>CURP: Contenido de la CURP</p>
               <div className="mt-8">
-                <Button startContent={<span className="material-symbols-outlined">
-                  lock
-                </span>} className="bg-white drop-shadow-md">
+                <Button
+                  startContent={<span className="material-symbols-outlined">lock</span>}
+                  className="bg-content1 drop-shadow-md"
+                >
                   Cambiar contraseña
                 </Button>
               </div>
@@ -32,26 +34,28 @@ export default function IndividualUserPage() {
               <p>Estado: Activo</p>
               <p>Usuario: Nombre de usuario</p>
               <div className="mt-8">
-                <Button startContent={<span className="material-symbols-outlined">
-                  account_tree
-                </span>} className="bg-white drop-shadow-md">
+                <Button
+                  startContent={<span className="material-symbols-outlined">account_tree</span>}
+                  className="bg-content1 drop-shadow-md"
+                >
                   Llevar a la estructura
                 </Button>
               </div>
             </div>
           </div>
         </div>
-        <div id="roles" className="flex flex-col mt-16 border shadow-lg p-8 rounded-xl">
+
+        <Divider className="my-10 max-w-5xl" />
+
+        <div id="roles" className="flex flex-col p-8 rounded-xl max-w-4xl border border-divider shadow-lg">
           <h1 className="text-4xl">Roles</h1>
-          <div className="flex flex-col gap-4 mt-4">
+          <div className="flex flex-col gap-6 mt-4">
             {numeros.map((numero) => (
-              <div key={numero}>
-                <Roles key={numero} numero={numero} />
-              </div>
+              <Roles key={numero} numero={numero} />
             ))}
           </div>
         </div>
-      </div>
+      </main>
     </div>
   );
 }
--- src/components/Roles.tsx
+++ src/components/Roles.tsx
@@ -2,56 +2,56 @@ import React, { useEffect } from "react";
 import { Button, ButtonGroup, Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Selection } from "@nextui-org/react";
 
 export default function Roles({ numero }: { children?: React.ReactNode; numero?: number }) {
-    const [selectedOption, setSelectedOption] = React.useState<Selection>(new Set(["admin"]));
+  const [selectedOption, setSelectedOption] = React.useState<Selection>(new Set(["admin"]));
 
-    useEffect (() => {
-        console.log(selectedOption);
-        
-    },[selectedOption])
+  useEffect(() => {
+    console.log(selectedOption);
+
+  }, [selectedOption]);
 
 
-    const labelsMap = {
-        admin: "Admin",
-        viewer: "Viewer",
-        user: "User",
-    }
+  const labelsMap = {
+    admin: "Admin",
+    viewer: "Viewer",
+    user: "User",
+  };
 
-    // Convert the Set to an Array and get the first value.
-    const selectedOptionValue = Array.from(selectedOption)[0] as keyof typeof labelsMap;
+  // Convert the Set to an Array and get the first value.
+  const selectedOptionValue = Array.from(selectedOption)[0] as keyof typeof labelsMap;
 
-    return (
-        <div className="flex flex-row justify-between items-center">
-            <h1>Modulo {numero}</h1>
-            <ButtonGroup variant="flat">
-                <Button>{labelsMap[selectedOptionValue]}</Button>
-                <Dropdown placement="bottom-end">
-                    <DropdownTrigger>
-                        <Button isIconOnly>
-                            <span className="material-symbols-outlined">
-                                expand_more
-                            </span>
-                        </Button>
-                    </DropdownTrigger>
-                    <DropdownMenu
-                        disallowEmptySelection
-                        aria-label="Merge options"
-                        selectedKeys={selectedOption}
-                        selectionMode="single"
-                        onSelectionChange={setSelectedOption}
-                        className="max-w-[300px]"
-                    >
-                        <DropdownItem key="admin">
-                            {labelsMap["admin"]}
-                        </DropdownItem>
-                        <DropdownItem key="viewer">
-                            {labelsMap["viewer"]}
-                        </DropdownItem>
-                        <DropdownItem key="user">
-                            {labelsMap["user"]}
-                        </DropdownItem>
-                    </DropdownMenu>
-                </Dropdown>
-            </ButtonGroup>
-        </div>
-    );
+  return (
+    <div className="flex flex-row justify-between items-center px-4">
+      <h1>Modulo {numero}</h1>
+      <ButtonGroup variant="flat">
+        <Button>{labelsMap[selectedOptionValue]}</Button>
+        <Dropdown placement="bottom-end">
+          <DropdownTrigger>
+            <Button isIconOnly>
+              <span className="material-symbols-outlined">
+                expand_more
+              </span>
+            </Button>
+          </DropdownTrigger>
+          <DropdownMenu
+            disallowEmptySelection
+            aria-label="Merge options"
+            selectedKeys={selectedOption}
+            selectionMode="single"
+            onSelectionChange={setSelectedOption}
+            className="max-w-[300px]"
+          >
+            <DropdownItem key="admin">
+              {labelsMap["admin"]}
+            </DropdownItem>
+            <DropdownItem key="viewer">
+              {labelsMap["viewer"]}
+            </DropdownItem>
+            <DropdownItem key="user">
+              {labelsMap["user"]}
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </ButtonGroup>
+    </div>
+  );
 }
