0659af1921634c0cf89d65f5dc162db49eee29b7
noelvazquez17 <73671246+noelvazquez17@users.noreply.github.com>
2024-04-07 00:55:14 -0600

    Clear code in Individual user page and use component ChangePasswordModal and modify user roles view

--- src/app/dashboard/base/users/[id]/page.tsx
+++ src/app/dashboard/base/users/[id]/page.tsx
@@ -2,17 +2,15 @@
 
 import React, { useEffect, useState } from "react";
 import Header from "@/components/Header";
-import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, Divider, Image, useDisclosure, Input } from "@nextui-org/react";
+import { Button, Divider, Image } from "@nextui-org/react";
 import Roles from "@/components/Roles";
 import { User, Person } from "@prisma/client";
+import { UserRoles } from "@/configs/roles";
+import ChangePasswordModal from "@/components/ChangePasswordModal";
 
 export default function IndividualUserPage({ params }: { params: { id: string } }) {
   const id = params.id;
-  const [user, setUser] = useState<User & { Person: Person, activeModules: number } | null>(null);
-  const [newPassword, setNewPassword] = useState("");
-  const [confirmationPassword, setConfirmationPassword] = useState("");
-  const { isOpen, onOpen, onOpenChange } = useDisclosure();
-  const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const [user, setUser] = useState<User & { Person: Person, activeModules: number, roles: UserRoles } | null>(null);
   const fullName = `${user?.Person.name} ${user?.Person.fatherLastName} ${user?.Person.motherLastName}`;
 
   const getUser = async () => {
@@ -22,33 +20,6 @@ export default function IndividualUserPage({ params }: { params: { id: string } 
       alert("Error al cargar el usuario");
     }
     setUser(resBody.data);
-  };
-
-  // Change password
-  const handleChangePassword = async () => {
-    const reqBody = { password: newPassword };
-
-    if (newPassword === ""){
-      alert("La contraseña no puede estar vacía");
-      return;
-    }
-
-    const resBody = await fetch(`/dashboard/api/users/${id}/changePassword`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(reqBody),
-    }).then((res) => res.json());
-
-    if (resBody.code !== "OK") {
-      alert("Error al cambiar la contraseña");
-    }
-    // close modal and clear input
-    onOpenChange();
-    alert("Contraseña cambiada correctamente");
-    setNewPassword("");
-    setConfirmationPassword("");
   };
 
   // Function to format phone number
@@ -90,22 +61,7 @@ export default function IndividualUserPage({ params }: { params: { id: string } 
                   <p><strong>Número telefónico:</strong> {formatPhone(user.Person.phone?.number || "")}</p>
                   <p><strong>CURP:</strong> {user.Person.curp}</p>
                   <div className="mt-8">
-                    <Button
-                      startContent={<span className="material-symbols-outlined">lock</span>}
-                      className="bg-content1 drop-shadow-md"
-                      onPress={onOpen}
-                    >
-                      Cambiar contraseña
-                    </Button>
-                    <ChangePasswordModal 
-                      isOpen={isOpen} 
-                      onOpenChange={onOpenChange} 
-                      newPassword={newPassword} 
-                      setNewPassword={setNewPassword} 
-                      handleChangePassword={handleChangePassword} 
-                      confirmationPassword={confirmationPassword}
-                      setConfirmationPassword={setConfirmationPassword}
-                    />
+                    <ChangePasswordModal id={id} />
                   </div>
                 </div>
                 <div className="flex flex-col gap-4">
@@ -129,9 +85,7 @@ export default function IndividualUserPage({ params }: { params: { id: string } 
             <div id="roles" className="flex flex-col p-8 rounded-xl max-w-4xl border border-divider shadow-lg">
               <h1 className="text-4xl">Roles</h1>
               <div className="flex flex-col gap-6 mt-4">
-                {numeros.map((numero) => (
-                  <Roles key={numero} numero={numero} />
-                ))}
+                <Roles userRoles={user.roles} />
               </div>
             </div>
           </main>
@@ -139,53 +93,5 @@ export default function IndividualUserPage({ params }: { params: { id: string } 
       }
     </div>
 
-  );
-}
-
-function ChangePasswordModal({ isOpen, onOpenChange, newPassword, setNewPassword, confirmationPassword, setConfirmationPassword, handleChangePassword }:
-  { isOpen: boolean, onOpenChange: () => void, newPassword: string, setNewPassword: (newPassword: string) => void, confirmationPassword: string, setConfirmationPassword: (confirmationPassword: string) => void, handleChangePassword: () => void }) {
-  return (
-    <Modal
-      isOpen={isOpen}
-      onOpenChange={onOpenChange}
-      placement="top-center"
-    >
-      <ModalContent>
-        {(onClose) => (
-          <>
-            <ModalHeader className="flex flex-col gap-1">Cambiar contraseña</ModalHeader>
-            <ModalBody>
-              <Input
-                autoFocus
-                label="Nueva contraseña"
-                placeholder="Ingrese la nueva contraseña"
-                type="password"
-                variant="bordered"
-                value={newPassword}
-                onChange={(e) => setNewPassword(e.target.value)}
-              />
-              <Input
-                label="Confirmar contraseña"
-                placeholder="Confirme la nueva contraseña"
-                type="password"
-                variant="bordered"
-                value={confirmationPassword}
-                onChange={(e) => setConfirmationPassword(e.target.value)}
-                errorMessage={newPassword !== confirmationPassword ? "Las contraseñas no coinciden" : ""}
-                color={newPassword === "" ? "default" : newPassword !== confirmationPassword ? "danger" : "success"}
-              />
-            </ModalBody>
-            <ModalFooter>
-              <Button color="default" variant="flat" onPress={onClose}>
-                Cancelar
-              </Button>
-              <Button color="primary" onPress={handleChangePassword} isDisabled={newPassword  === "" || newPassword !== confirmationPassword}>
-                Cambiar contraseña
-              </Button>
-            </ModalFooter>
-          </>
-        )}
-      </ModalContent>
-    </Modal>
   );
 }
