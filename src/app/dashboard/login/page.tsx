f375ffd1322f08b4903fa2b31050e336f71a1892
ElYizuz29 <amorales130@alumnos.uaq.mx>
2024-04-07 11:47:36 -0600

    Some things were resized, skeletons were added fot he footer, and the button error vas corrected

--- src/app/dashboard/login/page.tsx
+++ src/app/dashboard/login/page.tsx
@@ -1,9 +1,9 @@
 'use client'
 
 import React, { useContext, useState } from 'react'
-import { Link, Input, Button } from "@nextui-org/react";
+import { Link, Input, Button, Skeleton } from "@nextui-org/react";
 import Image from "next/image"
-import Wave from "../../../../public/Ola-2.svg"
+import Wave from "../../../../public/Ola-3.svg"
 
 export default function Login() {
 
@@ -41,8 +41,8 @@ export default function Login() {
       <div className="p-4">
         <header className="bg-background flex justify-between items-center min-h-12">
           <div className='flex flex-row gap-4 items-center'>
-            <div id="image" className="w-24 h-24 bg-blue-200"></div>
-            <h1 className="text-4xl font-normal text-[#113A5D]">Caminantes</h1>
+            <div id="image" className="w-16 h-16 bg-blue-200"></div>
+            <h1 className="text-3xl font-normal text-[#113A5D]">Caminantes</h1>
           </div>
 
           {/* Development Credits */}
@@ -50,15 +50,16 @@ export default function Login() {
             Desarrollado por <Link href="https://bout.sh" size="sm" className="text-gray-400" isExternal showAnchorIcon>Bout</Link>
           </span>
         </header>
-        <div id='main' className='flex flex-row gap-24 py-9 px-16 w-full'>
+        <div id='main' className='flex flex-row justify-between py-12 px-16 w-full'>
           <div id='form' className='flex flex-col gap-4 w-2/6'>
-            <p className='text-3xl text-[#113A5D]'>Inicia sesión</p>
+            <p className='text-2xl text-[#113A5D]'>Inicia sesión</p>
             <form className='flex flex-col gap-8'>
               <Input
                 type="user"
                 label="Usuario"
                 placeholder='Usuario'
                 labelPlacement='outside'
+                size='sm'
                 // value={userName}
                 // onValueChange={setUserName}
                 autoComplete="username"
@@ -70,17 +71,18 @@ export default function Login() {
                 label="Contraseña"
                 placeholder='Contraseña'
                 labelPlacement='outside'
+                size='sm'
                 // value={password}
                 // onValueChange={setPassword}
                 autoComplete="current-password"
                 isRequired
               />
-              <Button className='py-3 px-4 bg-[#FF3EA5] text-white w-1/4' type='submit'>
+              <Button className='py-2 px-3 bg-[#FF3EA5] text-white w-28 md:w-32' type='submit'>
                 Iniciar Sesión
               </Button>
             </form>
           </div>
-          <div id="image" className="w-3/5 bg-blue-200"></div>
+          <div id="image" className="w-3/6 bg-blue-200"></div>
         </div>
       </div>
       <footer>
@@ -89,9 +91,23 @@ export default function Login() {
           width={10}
           height={10}
           alt="Decoración"
-          className='w-screen h-40'
+          className='w-screen'
         />
-        <div className='flex flex-row justify-between bg-[#163172] h-12 p-8'></div>
+        <div className='flex flex-row justify-between bg-[#163172] h-screen p-4 -mt-3'>
+          <div className='flex flex-col gap-2'>
+            <Skeleton className="h-3 w-36 rounded-lg bg-slate-50" />
+            <Skeleton className="h-3 w-32 rounded-lg bg-slate-50" />
+          </div>
+          <div className='flex flex-col gap-2 text-center items-center'>
+            <p className='text-xl text-slate-50'>Caminantes</p>
+            <Skeleton className="h-3 w-36 rounded-lg bg-slate-50" />
+            <Skeleton className="h-3 w-32 rounded-lg bg-slate-50" />
+          </div>
+          <div className='flex flex-col gap-2 items-end'>
+            <Skeleton className="h-3 w-36 rounded-lg bg-slate-50" />
+            <Skeleton className="h-3 w-32 rounded-lg bg-slate-50" />
+          </div>
+        </div>
       </footer>
     </div>
   );
