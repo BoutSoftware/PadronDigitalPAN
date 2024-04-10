83f5de10a5fdc4da213d8d5e72d78b6d91f63b28
noelvazquez17 <73671246+noelvazquez17@users.noreply.github.com>
2024-04-10 15:46:01 -0600

    Refactor GET function in persons route to handle user parameter

--- src/app/dashboard/api/persons/route.ts
+++ src/app/dashboard/api/persons/route.ts
@@ -6,36 +6,44 @@ import { Person } from "@prisma/client";
 // Ejemplo de como acceder a la ruta GET de la API: http://localhost:3000/api/persons?name=[name]
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
-  const name = removeAccents(searchParams.get("name") || "");
+  const name = removeAccents(searchParams.get("name") || "") || null;
+  const user = searchParams.get("user");
 
-  // Validate params
-  // if (!name || typeof name !== "string") {
-  //   return new Response("Bad Request", { status: 400 });
-  // }
+  try {
 
-  // separate name by spaces
-  const nameParts = name.split(" ");
+    // Validate params
+    // if (!name || typeof name !== "string") {
+    //   return new Response("Bad Request", { status: 400 });
+    // }
 
-  // Search for persons 
-  const persons = await prisma.person.findMany({
-    where: {
-      AND:
-        nameParts.map(namePart => ({
+    // separate name by spaces
+    const nameParts = (name ?? "").split(" ");
+
+    // Search for persons 
+    const persons = await prisma.person.findMany({
+      where: {
+        // if user is false, search for persons that are not users
+        User: user === "false" ? null : user === "true" ? { id: { not: undefined } } : undefined,
+        AND: nameParts.map(namePart => ({
           OR: [
             { name: { contains: namePart, mode: "insensitive" } },
             { fatherLastName: { contains: namePart, mode: "insensitive" } },
             { motherLastName: { contains: namePart, mode: "insensitive" } }
           ]
         }))
+      }
+    });
+
+    // if no persons found 
+    if (!persons.length) {
+      return NextResponse.json({ code: "NOT_FOUND", message: "No persons found" });
     }
-  });
 
-  // if no persons found 
-  if (!persons.length) {
-    return NextResponse.json({ code: "NOT_FOUND", message: "No persons found" });
+    return NextResponse.json({ code: "OK", message: "Persons retrieved successfully", data: persons });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ code: "ERROR", message: "An error occurred" });
   }
-
-  return NextResponse.json({ code: "OK", message: "Persons retrieved successfully", data: persons });
 }
 
 export async function POST(request: NextRequest) {
