766abb379d58fe632b4943e833c70f72e2179f83
noelvazquez17 <73671246+noelvazquez17@users.noreply.github.com>
2024-04-11 00:57:49 -0600

    Add duplicates validation to curp, email, voterKey and rfc. Add validation email format, and refactor phone object to get only number to frontend

--- src/app/dashboard/api/directory/persons/route.ts
+++ src/app/dashboard/api/directory/persons/route.ts
@@ -9,7 +9,6 @@ export async function GET(request: NextRequest) {
   const user = searchParams.get("user");
 
   try {
-
     // Validate params
     // if (!name || typeof name !== "string") {
     //   return new Response("Bad Request", { status: 400 });
@@ -48,11 +47,50 @@ export async function GET(request: NextRequest) {
 export async function POST(request: NextRequest) {
   try {
     const { name, fatherLastName, motherLastName, birthPlace, email, curp, gender, phone, profession,
-      rfc, scholarship, tagIDs, voterKey, address } = (await request.json()) as (Person & { address: (Address & { pendingAddress?: PendingAddress }) });
+      rfc, scholarship, tagIDs, voterKey, address } = (await request.json()) as (Person & { phone: string | null, address?: (Address & { pendingAddress?: PendingAddress }) });
 
     // check for missing fields
     if (hasIncompleteFields({ name, fatherLastName })) {
       return NextResponse.json({ code: "INCOMPLETE_FIELDS", message: "Some fields are missing" });
+    }
+
+    const duplicates = await prisma.person.findMany({
+      where: {
+        OR: [
+          { curp: curp || undefined },
+          { email: email || undefined },
+          { rfc: rfc || undefined },
+          { voterKey: voterKey || undefined }
+        ]
+      }
+    });
+
+    // TODO: Enabled duplicates validation (remove !duplicates.length condition)
+    if (duplicates.length && !duplicates.length) {
+      // validate curp
+      if (curp && duplicates.find(d => d.curp === curp)) {
+        return NextResponse.json({ code: "DUPLICATE_CURP", message: "CURP already exists" });
+      }
+
+      // validate email
+      if (email && duplicates.find(d => d.email === email)) {
+        return NextResponse.json({ code: "DUPLICATE_EMAIL", message: "Email already exists" });
+      }
+
+      // validate rfc
+      if (rfc && duplicates.find(d => d.rfc === rfc)) {
+        return NextResponse.json({ code: "DUPLICATE_RFC", message: "RFC already exists" });
+      }
+
+      // validate voterKey
+      if (voterKey && duplicates.find(d => d.voterKey === voterKey)) {
+        return NextResponse.json({ code: "DUPLICATE_VOTERKEY", message: "Voter Key already exists" });
+      }
+    }
+
+    // validate email format
+    if (email && !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+      return NextResponse.json({ code: "INVALID_EMAIL", message: "Invalid email format" });
     }
 
     const person = await prisma.person.create({
@@ -64,7 +102,7 @@ export async function POST(request: NextRequest) {
         birthPlace,
         curp,
         rfc,
-        phone,
+        phone: phone ? { number: phone } : null,
         profession,
         scholarship,
         gender,
@@ -74,32 +112,34 @@ export async function POST(request: NextRequest) {
     });
 
     // create user Address
-    const personAddress = await prisma.address.create({
-      data: {
-        complementId: address.complementId,
-        defComplement: address.defComplement,
-        electoralSectionId: address.electoralSectionId,
-        interiorNum: address.interiorNum,
-        isEstablished: address.isEstablished,
-        outdoorNum: address.outdoorNum,
-        streetId: address.streetId,
-        foreignAddress: address.foreignAddress,
-        personId: person.id,
-      }
-    });
+    if (address) {
+      const personAddress = await prisma.address.create({
+        data: {
+          complementId: address.complementId,
+          defComplement: address.defComplement,
+          electoralSectionId: address.electoralSectionId,
+          interiorNum: address.interiorNum,
+          isEstablished: address.isEstablished,
+          outdoorNum: address.outdoorNum,
+          streetId: address.streetId,
+          foreignAddress: address.foreignAddress,
+          personId: person.id,
+        }
+      });
 
-    // if address in not established create a pendingAddress
-    if (!address.isEstablished) {
-      if (address.pendingAddress) {
-        await prisma.pendingAddress.create({
-          data: {
-            addressId: personAddress.id,
-            colonia: address.pendingAddress.colonia,
-            delgation: address.pendingAddress.delgation,
-            municipio: address.pendingAddress.municipio,
-            street: address.pendingAddress.street
-          }
-        });
+      // if address in not established create a pendingAddress
+      if (!address.isEstablished) {
+        if (address.pendingAddress) {
+          await prisma.pendingAddress.create({
+            data: {
+              addressId: personAddress.id,
+              colonia: address.pendingAddress.colonia,
+              delgation: address.pendingAddress.delgation,
+              municipio: address.pendingAddress.municipio,
+              street: address.pendingAddress.street
+            }
+          });
+        }
       }
     }
 
