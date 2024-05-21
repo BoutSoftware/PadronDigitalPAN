e2156ebeb4a380e8b34416885a6c1a7697f8673e
noelvazquez17 <73671246+noelvazquez17@users.noreply.github.com>
2024-05-21 14:02:38 -0600

    Add DELETE route for user deletion udating active state

--- src/app/dashboard/api/users/[id]/route.ts
+++ src/app/dashboard/api/users/[id]/route.ts
@@ -38,3 +38,44 @@ export async function GET(request: NextRequest, { params }: { params: { id: stri
     return NextResponse.json({ code: "OK", message: "An error occurred" });
   }
 }
+
+export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
+  try {
+    const id = params.id;
+
+    if (hasIncompleteFields({ id })) {
+      return NextResponse.json({ code: "INCOMPLETE_FIELDS", message: "Some fields are missing" });
+    }
+
+    const user = await prisma.user.findFirst({
+      where: {
+        id
+      }
+    });
+
+    if (!user) {
+      return NextResponse.json({ code: "NOT_FOUND", message: "User not found" });
+    }
+
+    // update user state
+    await prisma.user.update({
+      where: {
+        id
+      },
+      data: {
+        active: false
+      }
+    });
+
+    const data = {
+      ...user,
+      password: undefined,
+    };
+
+    return NextResponse.json({ code: "OK", message: "User deleted succesfully", data: data });
+
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ code: "ERROR", message: "An error occurred" });
+  }
+}
