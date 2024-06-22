5c9020102890793192e4ccee882316ec11ca6a42
Costas Rueda Juan Pablo <jcostas29@alumnos.uaq.mx>
2024-06-22 04:23:46 -0600

    Update active

---
+++ src/app/dashboard/api/visor/teams/[id]/members/[id]/route.ts
@@ -0,0 +1,27 @@
+import prisma from "@/configs/database";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(request: NextRequest, { params }: { params: { id: string }}) {
+  try {
+
+    const id = params.id;
+
+    const { newStatus } = await request.json() as { newStatus: boolean | null };
+
+    const memberUpdated = await prisma.visor_Caminantes.update({
+      where: {
+        id
+      },
+      data: {
+        // if newStatus is null, invert current status
+        active: newStatus === null ? !(await prisma.visor_Caminantes.findFirst({ where: { id }}))?.active : newStatus
+      }
+    });
+
+
+    return NextResponse.json({ code: "OK", message: "Member updated succesfully", data: memberUpdated });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ code: "ERROR", message: "An error occurred" });
+  }
+}
