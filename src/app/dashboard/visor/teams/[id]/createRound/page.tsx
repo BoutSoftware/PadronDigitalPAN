1c50a3589b8c36f537f067f5db840022994fffae
Diego <diegogama8934>
2024-07-04 00:40:09 -0600

    ADD: create markers with click

--- src/app/dashboard/visor/teams/[id]/createRound/page.tsx
+++ src/app/dashboard/visor/teams/[id]/createRound/page.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/ban-types */
 "use client";
 import { Select, SelectItem, Input, Button } from "@nextui-org/react";
-import { Dispatch, SetStateAction, useState } from "react";
+import { Dispatch, MouseEvent, SetStateAction, useContext, useEffect, useLayoutEffect, useRef, useState } from "react";
+import { mapContext } from "@/components/visor/maps/Map";
 import Map from "@/components/visor/maps/Map";
 import Marker from "@/components/visor/maps/Marker";
 import PopUp from "@/components/visor/maps/PopUp";
@@ -10,17 +12,89 @@ interface Point {
   lat: number
   lng: number
 }
+interface MapMouseEvent {
+  latLng: LatLng
+  domEvent: MouseEvent
+  pixel: Pixel
+  fi: Fi
+}
+interface LatLng {
+  lat: Function
+  lng: Function
+}
+interface Pixel {
+  x: number
+  y: number
+}
+interface Fi {
+  x: number
+  y: number
+}
 
 export default function CreateRoundPage() {
 
   const [idCounter, setIdCounter] = useState(0);
   const [points, setPoints] = useState<Point[]>([
-    // { id: "1", lat: 18.89425768444393, lng: -96.9345266598625 },
+    { id: "1", lat: 18.89425768444393, lng: -96.9345266598625 },
     { id: "2", lat: 18.90862561562622, lng: -96.9323472494062 },
-    // { id: "3", lat: 18.908715061343514, lng: -96.93247331323172 },
-    // { id: "4", lat: 18.908726479942146, lng: -96.93254640343007 },
-    // { id: "5", lat: 18.908701739644577, lng: -96.93262284638736 }
+    { id: "3", lat: 18.908715061343514, lng: -96.93247331323172 },
+    { id: "4", lat: 18.908726479942146, lng: -96.93254640343007 },
+    { id: "5", lat: 18.908701739644577, lng: -96.93262284638736 }
   ]);
+
+  // const { map } = useContext(mapContext);
+  // const mapRef = useRef<google.maps.Map | null>(null);
+  /*
+    !IMPORTANT (for me Xd): const map of useContext(mapContext) is always null.
+    !So we can´t use it to set the map listeners
+        
+      I think is because the map context is not wrapping
+      this page. So React can´t find a context to consume.
+
+      I will try using a useRef.
+
+    !IMPORTANT: useRef neither works.
+
+      I thing is because, even when I specified that mapRef.current is of type
+      google.maps.Map, React recognizes it like a HTML element. So when I do:
+
+      mapRef.current.addListener()
+
+      It throws an error that say that addListener is not a function (HTML elements)
+      don´t have addListeners, it has addEventListener().
+
+      I will try to modify map component to receive the listeners when it´s called
+  */
+
+  // useLayoutEffect(() => {
+  // initMapListeners();
+  // mapRef.current = document.getElementById("map") as unknown as google.maps.Map;
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
+
+  // function initMapListeners() {
+
+  // }
+
+  useEffect(() => {
+    console.log(points);
+  }, [points]);
+
+  function handleClick(e: MapMouseEvent) {
+    setPoints(prevPoints => {
+      return (
+        [
+          ...prevPoints,
+          {
+            id: (prevPoints.length + 1).toString(),
+            lat: e.latLng.lat(),
+            lng: e.latLng.lng()
+          }
+        ]
+      );
+    });
+  }
+
 
   return (
     <div className="p-4 flex gap-4 flex-1">
@@ -62,7 +136,9 @@ export default function CreateRoundPage() {
 
       </div>
 
-      <Map className="flex flex-1">
+      <Map
+        className="flex flex-1"
+        onClick={handleClick}>
         {
           points.map(({ id, lat, lng }, index) => (
             <Marker
--- src/components/visor/maps/Map.tsx
+++ src/components/visor/maps/Map.tsx
@@ -1,5 +1,5 @@
+/* eslint-disable @typescript-eslint/ban-types */
 "use client";
-
 import { GCP_API_KEY } from "@/configs";
 import { Loader } from "@googlemaps/js-api-loader";
 import { createContext, useEffect, useRef, useState } from "react";
@@ -11,20 +11,32 @@ interface MapContext {
   loader: Loader | null;
   map: google.maps.Map | null;
 }
+interface Props {
+  children: React.ReactNode,
+  center?: { lat: number, lng: number },
+  zoom?: number,
+  className?: string
+  onClick?: Function
+}
+
 export const mapContext = createContext<MapContext>({
   loader: null,
   map: null,
 });
 
-export default function Map({ children, center = defaultMapCenter, zoom = defaultMapZoom, className }: { children: React.ReactNode, center?: { lat: number, lng: number }, zoom?: number, className?: string }) {
+export default function Map({
+  children,
+  center = defaultMapCenter,
+  zoom = defaultMapZoom,
+  className,
+  onClick
+}: Props) {
   const mapRef = useRef<HTMLDivElement>(null);
   const [loader, setLoader] = useState<Loader | null>(null);
   const [map, setMap] = useState<google.maps.Map | null>(null);
 
   useEffect(() => {
     const initMap = async () => {
-      //console.log("Initializing Map");
-
       const loader = new Loader({
         apiKey: GCP_API_KEY,
         version: "weekly",
@@ -38,65 +50,12 @@ export default function Map({ children, center = defaultMapCenter, zoom = defaul
         mapId: "mainMap",
         clickableIcons: false,
         mapTypeControl: false,
-        streetViewControl: false,
+        streetViewControl: false
       });
-
-      const flightPlanCoordinates = [
-        { lat: 20.69661181570421, lng: -99.69102603354762 },
-        { lat: 21.018073, lng: -101.257358 },
-        { lat: 22.156469, lng: -100.985540 },
-        { lat: 20.479879, lng: -98.882404 },
-        { lat: 19.700781, lng: -101.184418 }
-      ];
-      const flightPath = new google.maps.Polyline({
-        path: flightPlanCoordinates,
-        geodesic: true,
-        strokeColor: "#f31260",
-        strokeOpacity: 1.0,
-        strokeWeight: 2,
-        editable: true,
-        draggable: true
-      });
-
-      flightPath.setMap(myMap);
-
-      // Define the LatLng coordinates for the polygon's path.
-      const triangleCoords = [
-        { lat: 25.774, lng: -80.19 },
-        { lat: 18.466, lng: -66.118 },
-        { lat: 32.321, lng: -64.757 },
-        { lat: 25.774, lng: -80.19 },
-      ];
-
-      // GeoJsonQro.features.map((municipioInfo) => {
-      //   console.log(municipioInfo.properties.Name2);
-      // });
-      // console.log(QroCoords);
-
-      // const municipalityCoords = GeoJsonQro.features[17].geometry.coordinates.map(coor => ({ lat: coor[1], lng: coor[0] }));
-
-      // const AmealcoPolygon = new google.maps.Polygon({
-      //   paths: municipalityCoords,
-      //   strokeColor: "#00155E",
-      //   strokeOpacity: 0.8,
-      //   strokeWeight: 5,
-      //   fillColor: "#00155E",
-      //   fillOpacity: 0.20
-      // });
-      //AmealcoPolygon.setMap(myMap);
-      // Construct the polygon.
-      const bermudaTriangle = new google.maps.Polygon({
-        paths: triangleCoords,
-        strokeColor: "#FF0000",
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: "#FF0000",
-        fillOpacity: 0.35,
-      });
-
-      bermudaTriangle.setMap(myMap);
-
-
+      if (onClick) {
+        console.log(onClick);
+      }
+      initListeners(myMap);
       setLoader(loader);
       setMap(myMap);
     };
@@ -104,6 +63,9 @@ export default function Map({ children, center = defaultMapCenter, zoom = defaul
     initMap();
   }, [center, zoom]);
 
+  const initListeners = (myMap: google.maps.Map | null) => {
+    if (onClick) myMap?.addListener("click", (e: MouseEvent) => onClick(e));
+  };
   return (
     <>
       <div id="map" className={className} ref={mapRef} />
