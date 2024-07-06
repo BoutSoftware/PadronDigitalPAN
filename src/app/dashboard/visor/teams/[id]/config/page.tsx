1bc3149ec76ac547dbf1f9814e061269bd056fbe
Diego <diegogama8934>
2024-07-05 19:41:25 -0600

    add: prototype of new ui working, implementation missing

--- src/app/dashboard/visor/teams/[id]/config/page.tsx
+++ src/app/dashboard/visor/teams/[id]/config/page.tsx
@@ -1,10 +1,9 @@
 "use client";
 import { Caminante } from "@/components/visor/individualTeam/Caminante";
-import { Input, Avatar, Button, Divider, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Selection, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Select, SelectItem, Autocomplete, AutocompleteItem } from "@nextui-org/react";
+import { Avatar, Button, Divider, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Selection, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Select, SelectItem, Autocomplete, AutocompleteItem } from "@nextui-org/react";
 import { useParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import { CONFIGURACIONES_GEOGRAFICAS, TIPOS_PUNTO } from "@/configs/catalogs/visorCatalog";
-import { ModalModifyGeographicArea } from "@/components/visor/individualTeam/ModalModifyGeographicArea";
 
 interface Structure {
   id: string
@@ -35,11 +34,7 @@ interface GeographicLevel {
   id: string
   nombre: string
 }
-interface GeographicLevelValue {
-  id: string
-  name: string
-}
-interface TeamResponse {
+interface Team {
   id: string
   name: string
   active: boolean
@@ -50,76 +45,94 @@ interface TeamResponse {
   TiposPunto: TipoPunto[]
   geographicConf: {
     geographicLevel: GeographicLevel
-    values: GeographicLevelValue[]
+    values: {
+      id: string
+      name: string
+    }[]
   }
 }
-
-interface TeamKeys {
+interface InputKeys {
   geographicLevelKey: Selection
   pointTypesKeys: Selection
+}
+interface GeographicValues {
+  values: string[]
+  selectedValues: any[]
 }
 
 export default function Page() {
 
-  const [membersAndConfig, setMembersAndConfig] = useState<TeamResponse | null>(null);
-  const [teamKeys, setTeamKeys] = useState<TeamKeys>({
-    geographicLevelKey: new Set(""),
-    pointTypesKeys: new Set([""])
-  });
-  const [geographicValues, setGeographicValues] = useState({
-    values: [],
-    selectedValues: []
-  });
+  const [team, setTeam] = useState<Team | null>(null);
+
+  const [inputKeys, setInputKeys] = useState<InputKeys>({ geographicLevelKey: new Set(""), pointTypesKeys: new Set([""]) });
+  const [geographicValues, setGeographicValues] = useState<GeographicValues>({ values: [], selectedValues: [] });
   const [isModalOpen, setIsModalOpen] = useState(false);
+
   const { id: teamId } = useParams();
 
-  async function handlePointTypeValue(key: Selection) {
+  useEffect(() => {
+    getAndSetTeamInfo();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    const resBody = await fetch(`/dashboard/api/visor/teams/${teamId}/pointTypes`, {
-      method: "PUT",
-      body: JSON.stringify({ pointTypesIDs: Array.from(teamKeys.pointTypesKeys) })
-    })
-      .then(res => res.json())
-      .catch(err => console.error(err));
-
-    console.log(resBody);
-  }
+  useEffect(() => {
+    console.log({ inputKeys: inputKeys });
+    console.log({ geographicValues: geographicValues });
+    console.log({ team: team });
+  }, [inputKeys, geographicValues, team]);
 
   async function getAndSetTeamInfo() {
 
-    // TODO: Refactor this
-
-    // Get team information
-    const teamResBody = await fetch(`/dashboard/api/visor/teams/${teamId}`)
-      .then(res => res.json())
-      .catch(err => console.error(err));
-    if (teamResBody.code !== "OK") {
-      console.error(teamResBody.message);
-      return;
-    }
-    const { data: teamData } = teamResBody;
-    console.log(teamData);
+    // General team info
+    const teamData = await getTeamInfo();
+    setTeam(teamData);
 
     // Get geographic level values
-    const geographicLevelId = teamData.geographicConf.geographicLevel.id; // Argument for function
-    const municipios = teamData.Auxiliary.municipios; // Argument for function
-    const geographicLevelData = await getGeographicLevelValues(geographicLevelId, municipios);
-    console.log(geographicLevelData);
-    setGeographicValues({
-      values: geographicLevelData,
-      selectedValues: teamData.geographicConf.values
-    });
+    const geoLevelId: string = teamData.geographicConf.geographicLevel.id; // Argument for function
+    const municipios: string[] = teamData.Auxiliary.municipios; // Argument for function
 
-    // Setting keys state
-    const initialGeographicConfKeys: TeamKeys = {
-      geographicLevelKey: new Set([geographicLevelId]),
+    const geoLevelValues = await getGeographicLevelValues(geoLevelId, municipios);
+    if (geoLevelValues) {
+      setGeographicValues({
+        values: geoLevelValues,
+        selectedValues: teamData.geographicConf.values
+      });
+    }
+
+    // Setting inputs keys
+    const initialInputKeys: InputKeys = {
+      geographicLevelKey: new Set([geoLevelId]),
       pointTypesKeys: new Set(teamData.TiposPunto.map((pointType: TipoPunto) => pointType.id))
     };
-    setTeamKeys(initialGeographicConfKeys);
-    setMembersAndConfig(teamData);
+    setInputKeys(initialInputKeys);
   }
 
-  async function getGeographicLevelValues(geographicLevelId: string, municipios: string[]) {
+  async function getTeamInfo() {
+    const resBody = await fetch(`/dashboard/api/visor/teams/${teamId}`)
+      .then(res => res.json())
+      .catch(err => console.error(err));
+    if (resBody.code !== "OK") {
+      console.error(resBody.message);
+      return;
+    }
+    return resBody.data;
+  }
+
+  async function handlePointTypeValue(key: Selection) {
+    const resBody = await fetch(`/dashboard/api/visor/teams/${teamId}/pointTypes`, {
+      method: "PUT",
+      body: JSON.stringify({ pointTypesIDs: Array.from(inputKeys.pointTypesKeys) })
+    })
+      .then(res => res.json())
+      .catch(err => console.error(err));
+    if (resBody.code !== "OK") {
+      console.error(resBody.message);
+      return;
+    }
+    console.log(resBody);
+  }
+
+  async function getGeographicLevelValues(geographicLevelId: string, municipios: string[]): Promise<void | string[]> {
     const params = `geographicLevel=${geographicLevelId}&municipios=${municipios.join(",")}`;
     const resBody = await fetch(`/dashboard/api/visor/geographicConfiguration/?${params}`)
       .then(res => res.json())
@@ -137,52 +150,45 @@ export default function Page() {
     const geographicLevelId = Array.from(key)[0];
     if (!geographicLevelId) return;
 
-    const municipios = membersAndConfig?.Auxiliary.municipios;
+    const municipios = team?.Auxiliary.municipios;
     const newGeographicValues = await getGeographicLevelValues(geographicLevelId.toString(), municipios);
 
-    // setGeographicValues();
+    if (!newGeographicValues) return;
+
+    setGeographicValues({ ...geographicValues, values: newGeographicValues });
   }
-
-  useEffect(() => {
-    console.log(teamKeys.geographicLevelKey);
-  }, [teamKeys.geographicLevelKey]);
-
-  useEffect(() => {
-    getAndSetTeamInfo();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   return (
     <div className="p-4 flex gap-4">
       <div className="flex flex-col flex-1 gap-4">
         <h2 className="text-2xl text-foreground-600 ">Estructura perteneciente</h2>
         <Divider />
-        <span className="text-foreground-400">{membersAndConfig?.Structure.nombre}</span>
+        <span className="text-foreground-400">{team?.Structure.nombre}</span>
         <div className="flex flex-col gap-2">
           <h2 className="text-2xl text-foreground-600 ">Auxiliar de coordinación</h2>
           <Divider />
           <div className="flex items-center gap-2">
-            <Avatar showFallback name={membersAndConfig?.Auxiliary.name} src="https://images.unsplash.com/broken" />
-            <span>{membersAndConfig?.Auxiliary.name}</span>
+            <Avatar showFallback name={team?.Auxiliary.name} src="https://images.unsplash.com/broken" />
+            <span>{team?.Auxiliary.name}</span>
           </div>
         </div>
         <div className="flex flex-col gap-2">
           <h2 className="text-2xl text-foreground-600 ">Enlace del equipo</h2>
           <Divider />
           <div className="flex items-center gap-2">
-            <Avatar showFallback name={membersAndConfig?.Link.name} src="https://images.unsplash.com/broken" />
-            <p>{membersAndConfig?.Link.name}</p>
+            <Avatar showFallback name={team?.Link.name} src="https://images.unsplash.com/broken" />
+            <p>{team?.Link.name}</p>
           </div>
         </div>
         <div className="flex flex-col gap-4">
           <h2 className="text-2xl text-foreground-600 ">
             Miembros
-            <span className="text-base text-foreground-400"> {membersAndConfig?.Caminantes.length} miembros en el equipo</span>
+            <span className="text-base text-foreground-400"> {team?.Caminantes.length} miembros en el equipo</span>
           </h2>
           <Divider />
           <div className="flex flex-col gap-4">
             {
-              membersAndConfig?.Caminantes.map(({ id, active, name }) => (
+              team?.Caminantes.map(({ id, active, name }) => (
                 <Caminante key={id} id={id} active={active} name={name} />
               ))
             }
@@ -205,7 +211,7 @@ export default function Page() {
                     <div className="flex flex-col gap-4">
                       <Select
                         label="Selecciona un nivel geográfico"
-                        selectedKeys={teamKeys.geographicLevelKey}
+                        selectedKeys={inputKeys.geographicLevelKey}
                         onSelectionChange={handleGeographicValueChange}
                         selectionMode="single"
                       >
@@ -217,17 +223,19 @@ export default function Page() {
                         label="Agrega valores"
                         shouldCloseOnBlur
                         menuTrigger="input"
+                        defaultItems={geographicValues.values}
+                        disabledKeys={geographicValues.selectedValues.map(val => val.id)}
                       >
                         {
-                          geographicValues.values.map((value) => (
+                          ((item) => (
                             <AutocompleteItem
-                              key={value.id}
+                              key={item.id}
                               onPress={() => setGeographicValues({
                                 ...geographicValues,
-                                selectedValues: [...geographicValues.selectedValues, value]
+                                selectedValues: [...geographicValues.selectedValues, item]
                               })}
                             >
-                              {value.name}
+                              {item.name}
                             </AutocompleteItem>
                           ))
                         }
@@ -236,7 +244,7 @@ export default function Page() {
                     <div className="flex flex-col flex-1 max-h-[400px] overflow-auto">
                       <h4 className="text-xl text-foreground-700">Valores actuales</h4>
                       {
-                        geographicValues.selectedValues.map((selectedValue: { id: string, name: string }, index) => (
+                        geographicValues.selectedValues.map((selectedValue, index) => (
                           <React.Fragment key={selectedValue.id}>
                             <div className="flex justify-between items-center w-full p-4">
                               <span>{selectedValue.name}</span>
@@ -269,7 +277,7 @@ export default function Page() {
           <Divider />
           <div className="flex flex-col gap-4">
             {
-              membersAndConfig?.geographicConf.values.map(({ id, name }) => (
+              team?.geographicConf.values.map(({ id, name }) => (
                 <div key={id} className="flex justify-between">
                   <div className="flex flex-col">
                     <span>{name}</span>
@@ -288,17 +296,17 @@ export default function Page() {
             </h2>
             <Dropdown>
               <DropdownTrigger>
-                <Button>{membersAndConfig?.TiposPunto.length} tipo{(membersAndConfig?.TiposPunto.length || 0) > 1 && "s"} de punto</Button>
+                <Button>{team?.TiposPunto.length} tipo{(team?.TiposPunto.length || 0) > 1 && "s"} de punto</Button>
                 {/* Need to code "|| 0" in the line below by a typescript error */}
               </DropdownTrigger>
               <DropdownMenu
                 aria-label="Tipos de punto"
-                selectedKeys={teamKeys.pointTypesKeys}
+                selectedKeys={inputKeys.pointTypesKeys}
                 selectionMode="single"
                 onSelectionChange={(key) => handlePointTypeValue(key)}>
                 {
                   TIPOS_PUNTO
-                    .filter(pointType => pointType.estructuraId == membersAndConfig?.Structure.id)
+                    .filter(pointType => pointType.estructuraId == team?.Structure.id)
                     .map(pointType => (
                       <DropdownItem key={pointType.id}>{pointType.nombre}</DropdownItem>
                     ))
@@ -309,7 +317,7 @@ export default function Page() {
           <Divider />
           <div className="flex flex-col gap-4">
             {
-              membersAndConfig?.TiposPunto.map(({ id, nombre }) => (
+              team?.TiposPunto.map(({ id, nombre }) => (
                 <div key={id} className="flex justify-between items-center">
                   <span>{nombre}</span>
                   <Button>Quitar</Button>
