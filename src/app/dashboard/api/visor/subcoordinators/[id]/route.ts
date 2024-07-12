947d5d9489c5f0081164aae03dc675b510e72768
noelvazquez17 <73671246+noelvazquez17@users.noreply.github.com>
2024-07-12 17:45:06 -0600

    feat: Delete subcoordinator by id and associated data
    It also deletes any associated auxiliaries, teams, and relationships. Additionally, it updates the active status and user titles, using api route to delete auxiliaries

--- src/app/dashboard/api/visor/subcoordinators/[id]/route.ts
+++ src/app/dashboard/api/visor/subcoordinators/[id]/route.ts
@@ -44,3 +44,65 @@ export async function PATCH(request: NextRequest, { params }: { params: { id: st
     return NextResponse.json({ code: "ERROR", message: "An error occurred" });
   }
 }
+
+// Delete subcoordinator by id
+export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
+  try {
+    // Get base url
+    const baseUrl = request.nextUrl.origin;
+    const id = params.id;
+    
+    const currentSubcoordinator = await prisma.visor_SubCoordinator.findUnique({
+      where: { id, active: true },
+      include: {
+        Auxiliaries: {
+          where: { active: true }
+        }
+      }
+    });
+
+    if (!currentSubcoordinator) {
+      return NextResponse.json({ code: "NOT_FOUND", message: "Subcoordinator not found" });
+    }
+
+    // Delete auxiliares, teams and relationships
+    if (currentSubcoordinator.Auxiliaries && currentSubcoordinator.Auxiliaries.length > 0) {
+      await Promise.all(currentSubcoordinator.Auxiliaries.map(async auxiliary => {
+        try {
+          const teamUrl = new URL(`/dashboard/api/visor/auxiliaries/${auxiliary.id}`, baseUrl).href;
+
+          const result = await fetch(teamUrl, { method: "DELETE" }).then(res => res.json());
+          if (result.code === "ERROR") {
+            return NextResponse.json({ code: "ERROR", message: `An error occurred while deleting auxiliary: ${auxiliary.id}` });
+          }
+        } catch (error) {
+          console.log(error);
+          return NextResponse.json({ code: "ERROR", message: "An error occurred while deleting auxiliaries" });
+        }
+      }));
+    }
+
+    // Update subcoordinator active status and user titles
+    const deletedResult = await prisma.visor_SubCoordinator.update({
+      where: { id },
+      data: {
+        active: false,
+        Technical: {
+          update: {
+            title: null
+          }
+        },
+        User: {
+          update: {
+            title: null
+          }
+        },
+      }
+    });
+
+    return NextResponse.json({ code: "OK", message: "Subcoordinator deleted successfully", data: deletedResult });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ code: "ERROR", message: "An error occurred" });
+  }
+}
