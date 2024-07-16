d5c1a50850acc0f30adb4a2145f456aabf1ee04a
noelvazquez17 <73671246+noelvazquez17@users.noreply.github.com>
2024-07-16 16:03:00 -0600

    refactor: Update caminantes route to include active flag in queries and remove commented code, change the way to get caminantes with team

--- src/app/dashboard/api/visor/caminantes/route.ts
+++ src/app/dashboard/api/visor/caminantes/route.ts
@@ -1,50 +1,6 @@
 import prisma from "@/configs/database";
 import { NextRequest, NextResponse } from "next/server";
 
-// Get caminantes without team if team could be null
-// export async function GET(request: NextRequest) {
-//   try {
-//     const searchParams = request.nextUrl.searchParams;
-//     const team = searchParams.get("team");
-
-//     const caminantes = await prisma.visor_Caminantes.findMany({
-//       where: {
-//         Team: team === "false" ? null : team === "true" ? { id: { not: undefined } } : undefined
-//       },
-//       select: {
-//         id: true,
-//         active: true,
-//         userId: true,
-//         User: {
-//           select: {
-//             User: {
-//               select: {
-//                 Person: {
-//                   select: {
-//                     name: true,
-//                     fatherLastName: true,
-//                     motherLastName: true
-//                   }
-//                 }
-//               }
-            
-//             }
-//           }
-//         }
-//       }
-//     });
-
-//     if (!caminantes.length) {
-//       return NextResponse.json({ code: "NOT_FOUND", message: "No caminantes found" });
-//     }
-
-//     return NextResponse.json({ code: "OK", message: "Caminantes retrieved successfully", data: caminantes });
-//   } catch (error) {
-//     console.log(error);
-//     return NextResponse.json({ code: "ERROR", message: "An error occurred" });
-//   }
-// }
-
 // Get caminantes without team (visor users with rol User)
 export async function GET(request: NextRequest) {
   try {
@@ -53,17 +9,13 @@ export async function GET(request: NextRequest) {
 
     const caminantes = await prisma.visor_User.findMany({
       where: {
+        active: true,
         rol: "User",
         ...(team === "false" && {
-          // Caminantes: { none: {} },
-          // Links: { none: {} }
           title: null
         }),
         ...(team === "true" && {
-          OR: [
-            { Caminantes: { some: {} } },
-            { Links: { some: {} } }
-          ]
+          title: { not: null }
         })
       },
       select: {
