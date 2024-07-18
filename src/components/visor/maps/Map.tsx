88adcd9938c5719600264eae13ea560b3ed5be6a
Diego Martinez <diegogama8934@gmail.com>
2024-07-18 13:27:27 -0600

    add: group of polygones drawed on map

--- src/components/visor/maps/Map.tsx
+++ src/components/visor/maps/Map.tsx
@@ -6,30 +6,61 @@ import { createContext, useEffect, useRef, useState } from "react";
 const defaultMapCenter = { lat: 20.84651181570421, lng: -99.79102603354762 };
 const defaultMapZoom = 9;
 
-const figures1Paths = [
-  // This could be electoral sections
-  // And need to be an array of arrays with the following data structure
-  { "lat": 37.7749, "lng": -122.4194 },
-  { "lat": 37.7849, "lng": -122.4094 },
-  { "lat": 37.7749, "lng": -122.3994 }
+const figures1Paths: latLng[][] = [
+  [
+    { "lat": 20.629196, "lng": -100.450957 },
+    { "lat": 20.588866, "lng": -100.444600 },
+    { "lat": 20.611346, "lng": -100.405750 }
+  ],
+  [
+    { "lat": 20.627213, "lng": -100.368313 },
+    { "lat": 20.594817, "lng": -100.349948 },
+    { "lat": 20.607380, "lng": -100.301209 }
+  ],
+  [
+    { "lat": 20.710486, "lng": -100.378202 },
+    { "lat": 20.706521, "lng": -100.320281 },
+    { "lat": 20.676786, "lng": -100.348535 }
+  ]
 ];
-const figures2Paths = [
-  // This could be electoral sections
-  // And need to be an array of arrays with the following data structure
-  { "lat": 37.7749, "lng": -122.4294 },
-  { "lat": 37.7749, "lng": -122.4194 },
-  { "lat": 37.7649, "lng": -122.4194 },
-  { "lat": 37.7649, "lng": -122.4294 }
+
+
+const figures2Paths: latLng[][] = [
+  [
+    { "lat": 20.652412, "lng": -100.389106 },
+    { "lat": 20.635589, "lng": -100.362217 },
+    { "lat": 20.601271, "lng": -100.389485 }
+  ],
+  [
+    { "lat": 20.667234, "lng": -100.353627 },
+    { "lat": 20.651109, "lng": -100.335341 },
+    { "lat": 20.620590, "lng": -100.367672 }
+  ],
+  [
+    { "lat": 20.676791, "lng": -100.317848 },
+    { "lat": 20.660768, "lng": -100.302926 },
+    { "lat": 20.639812, "lng": -100.328597 }
+  ]
 ];
-const figures3Paths = [
-  // This could be electoral sections
-  // And need to be an array of arrays with the following data structure
-  { "lat": 37.7849, "lng": -122.4194 },
-  { "lat": 37.7949, "lng": -122.4094 },
-  { "lat": 37.7949, "lng": -122.3994 },
-  { "lat": 37.7849, "lng": -122.3894 },
-  { "lat": 37.7749, "lng": -122.3994 }
+
+const figures3Paths: latLng[][] = [
+  [
+    { "lat": 20.630000, "lng": -100.360000 },
+    { "lat": 20.632000, "lng": -100.362000 },
+    { "lat": 20.628000, "lng": -100.362000 }
+  ],
+  [
+    { "lat": 20.632000, "lng": -100.362000 },
+    { "lat": 20.634000, "lng": -100.364000 },
+    { "lat": 20.630000, "lng": -100.364000 }
+  ],
+  [
+    { "lat": 20.634000, "lng": -100.364000 },
+    { "lat": 20.636000, "lng": -100.366000 },
+    { "lat": 20.632000, "lng": -100.366000 }
+  ]
 ];
+
 const polygonOptions = { // This must be the deseable style of the polygons
   strokeColor: "#FF0000",
   strokeOpacity: 0.8,
@@ -37,6 +68,10 @@ const polygonOptions = { // This must be the deseable style of the polygons
   fillColor: "#FF0000",
   fillOpacity: 0.35
 };
+interface latLng {
+  lat: number
+  lng: number
+}
 interface MapContext {
   loader: Loader | null;
   map: google.maps.Map | null;
@@ -102,9 +137,9 @@ export default function Map({
     if (!showPolygones) return;
     if (!map) return;
 
-    const figures1 = new google.maps.Polygon({ paths: figures1Paths, ...polygonOptions }); // This need to be google.maps.Polygon[]
-    const figures2 = new google.maps.Polygon({ paths: figures2Paths, ...polygonOptions }); // This need to be google.maps.Polygon[]
-    const figures3 = new google.maps.Polygon({ paths: figures3Paths, ...polygonOptions }); // This need to be google.maps.Polygon[]
+    const figures1 = figures1Paths.map(figure1path => new google.maps.Polygon({ paths: figure1path, ...polygonOptions }));
+    const figures2 = figures2Paths.map(figure2path => new google.maps.Polygon({ paths: figure2path, ...polygonOptions }));
+    const figures3 = figures3Paths.map(figure3path => new google.maps.Polygon({ paths: figure3path, ...polygonOptions }));
 
     const polygonsListener = map.addListener("zoom_changed", () => {
       drawPolygones(figures1, figures2, figures3);
@@ -113,9 +148,9 @@ export default function Map({
     return () => {
       console.log("Removing listeners...");
       google.maps.event.removeListener(polygonsListener);
-      figures1.setMap(null); // This need to map google.maps.Polygon[] and do figure => figure.setMap(null)
-      figures2.setMap(null); // This need to map google.maps.Polygon[] and do figure => figure.setMap(null)
-      figures3.setMap(null); // This need to map google.maps.Polygon[] and do figure => figure.setMap(null)
+      figures1.map(figure => figure.setMap(null));
+      figures2.map(figure => figure.setMap(null));
+      figures3.map(figure => figure.setMap(null));
     };
 
   }, [showPolygones, map]);
@@ -124,27 +159,34 @@ export default function Map({
     if (onClick) myMap?.addListener("click", (e: google.maps.MapMouseEvent) => onClick(e));
   };
 
-  const drawPolygones = (figures1: google.maps.Polygon, figures2: google.maps.Polygon, figures3: google.maps.Polygon) => {
-    const currentZoom = map!.getZoom();
+  const drawPolygones = (figures1: google.maps.Polygon[], figures2: google.maps.Polygon[], figures3: google.maps.Polygon[]) => {
 
+    if (!map) return;
+    const currentZoom = map.getZoom();
     if (!currentZoom) return;
 
     if (currentZoom >= 8 && currentZoom < 12) {
-      figures1.setMap(map);
-      figures2.setMap(null);
-      figures3.setMap(null);
+      figures1.map(f => f.setMap(map));
+      figures2.map(f => f.setMap(null));
+      figures3.map(f => f.setMap(null));
     }
 
     if (currentZoom >= 12 && currentZoom < 15) {
-      figures1.setMap(null);
-      figures2.setMap(map);
-      figures3.setMap(null);
+      figures1.map(f => f.setMap(null));
+      figures2.map(f => f.setMap(map));
+      figures3.map(f => f.setMap(null));
     }
 
     if (currentZoom >= 15 && currentZoom < 18) {
-      figures1.setMap(null);
-      figures2.setMap(null);
-      figures3.setMap(map);
+      figures1.map(f => f.setMap(null));
+      figures2.map(f => f.setMap(null));
+      figures3.map(f => f.setMap(map));
+    }
+
+    if (currentZoom < 8 || currentZoom >= 18) {
+      figures1.map(f => f.setMap(null));
+      figures2.map(f => f.setMap(null));
+      figures3.map(f => f.setMap(null));
     }
   };
   return (
