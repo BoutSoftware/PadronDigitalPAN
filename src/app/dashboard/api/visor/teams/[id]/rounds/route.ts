0b72bb2c2a2998742d4f954db3f353564e8696e3
JaviZamudio <javier.elihu100@gmail.com>
2024-07-18 14:13:26 -0600

    fully functional

--- src/app/dashboard/api/visor/teams/[id]/rounds/route.ts
+++ src/app/dashboard/api/visor/teams/[id]/rounds/route.ts
@@ -1,115 +1,139 @@
-import { STATUS_RONDAS } from "@/configs/catalogs/visorCatalog";
 import prisma from "@/configs/database";
 import { NextRequest, NextResponse } from "next/server";
+import { hasIncompleteFields } from "@/utils";
+import { STATUS_RONDAS } from "@/configs/catalogs/visorCatalog";
+import { Visor_CheckPoint } from "@prisma/client";
 
-// Delete round by id (Change active to false)
-export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
-  const id = params.id;
+// Create a round
+export async function POST(request: NextRequest, { params }: { params: { id: string } }) {
+  interface reqBody {
+    name: string;
+    startedAt: Date | undefined;
+    status: string | undefined;
+    pointTypesIDs: string[];
+    createdById: string;
+    checkPoints: {
+      latitude: number;
+      longitude: number;
+    }[];
+  }
 
   try {
-    const round = await prisma.visor_Round.findFirst({ where: { id, active: true } });
+    const reqBody = await request.json() as reqBody;
+    const { id } = params;
+    const { name, startedAt, pointTypesIDs, status, createdById, checkPoints } = reqBody;
 
-    if (!round) {
-      return NextResponse.json({ code: "NOT_FOUND", message: "Round not found" });
+    if (hasIncompleteFields({ name, pointTypesIDs, createdById, checkPoints })) {
+      return NextResponse.json({ code: "INCOMPLETE_FIELDS", message: "Some fields are missing" });
     }
 
-    // Update active and status
-    const roundDeleted = await prisma.visor_Round.update({
-      where: { id },
+    // Verify if team exists and is active
+    const teamExists = await prisma.visor_Team.findFirst({ where: { id, active: true } });
+
+    if (!teamExists) {
+      return NextResponse.json({ code: "TEAM_NOT_FOUND", message: "Team not found" });
+    }
+
+    // Verify if user exists
+    const userExists = await prisma.visor_User.findFirst({ where: { id: createdById } });
+    if (!userExists) {
+      return NextResponse.json({ code: "USER_NOT_FOUND", message: "User not found" });
+    }
+
+    // create round and checkpoints
+    const round = await prisma.visor_Round.create({
       data: {
-        active: false,
-        status: "terminada"
+        name,
+        teamId: id,
+        // If the round starts at the moment, the status that comes from the front is assigned
+        status: status ? status :
+          // If it is not started at the moment, the status of not started is added to the catalogs
+          STATUS_RONDAS.map((status) => status.id).find((id) => id === "noiniciada")!,
+        // If the round starts at the moment, the date received is set, otherwise it is set to null
+        startedAt: startedAt ? startedAt : null,
+        pointTypesIDs,
+        createdById,
+        CheckPoints: {
+          createMany: {
+            data: checkPoints.map((checkPoint) => ({
+              latitude: checkPoint.latitude,
+              longitude: checkPoint.longitude
+            }))
+          }
+        },
+      },
+      include: {
+        CheckPoints: true
       }
     });
 
-    return NextResponse.json({ code: "OK", message: "Round deleted successfully", data: roundDeleted });
+    return NextResponse.json({ code: "OK", message: "Round created successfully", data: round });
   } catch (error) {
+    console.error(error);
     return NextResponse.json({ code: "ERROR", message: "An error occurred" });
   }
 }
 
-export async function PATCH(request: NextRequest, { params }: { params: { id: string } }) {
+// Get paused rounds, active and no started rounds by team
+export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
+  type Round = {
+    id: string;
+    active: boolean;
+    name: string;
+    startedAt: Date | null;
+    status: string;
+    pointTypesIDs: string[];
+    CheckPoints: Visor_CheckPoint[];
+  };
+  interface TeamRounds {
+    active: Round[];
+    paused: Round[];
+    noStarted: Round[];
+  }
+
   try {
-    const id = params.id;
-    const { action } = await request.json() as { action: keyof typeof actionFunctions };
+    const { id } = params;
 
-    // action could be "start", "pause" and "stop"
+    // Verify if team exists and is active
+    const teamExists = await prisma.visor_Team.findFirst({ where: { id, active: true } });
 
-    if (!action) {
-      return NextResponse.json({ code: "INCOMPLETE_FIELDS", message: "Incomplete fields" });
+    if (!teamExists) {
+      return NextResponse.json({ code: "TEAM_NOT_FOUND", message: "Team not found" });
     }
 
-    const actionFunctions = {
-      start: startRound,
-      pause: pauseRound,
-      stop: stopRound
+    // Get paused rounds, active and no started rounds by team
+    const rounds = await prisma.visor_Round.findMany({
+      where: {
+        teamId: id,
+        active: true,
+        status: {
+          in: STATUS_RONDAS.map((status) => status.id).filter((id) => id !== "terminada")
+        }
+      },
+      select: {
+        id: true,
+        name: true,
+        active: true,
+        startedAt: true,
+        status: true,
+        pointTypesIDs: true,
+        CheckPoints: true
+      }
+    });
+
+    if (!rounds.length) {
+      return NextResponse.json({ code: "NOT_FOUND", message: "No rounds found" });
+    }
+
+    const teamRounds: TeamRounds = {
+      active: rounds.filter((round) => round.status === "activa"),
+      paused: rounds.filter((round) => round.status === "pausada"),
+      noStarted: rounds.filter((round) => round.status === "noiniciada")
     };
 
-    if (!(action in actionFunctions)) {
-      return NextResponse.json({ code: "INVALID_ACTION", message: "Action is not allowed, it must be: 'start', 'pause', 'stop'" });
-    }
-
-    const round = await actionFunctions[action](id);
-
-    if (!round) {
-      return NextResponse.json({ code: "NOT_FOUND", message: "Round not found" });
-    }
-
-    return NextResponse.json({ code: "OK", message: round.status, data: round });
+    return NextResponse.json({ code: "OK", message: "Rounds retrieved successfully", data: teamRounds });
   } catch (error) {
-    console.log(error);
+    console.error(error);
     return NextResponse.json({ code: "ERROR", message: "An error occurred" });
   }
-}
-
-async function startRound(id: string) {
-
-  // Check that there are no active rounds
-  const activeRounds = await prisma.visor_Round.aggregate({
-    _count: {
-      id: true,
-    },
-    where: {
-      active: true,
-      status: {
-        in: ["activa", "pausada"],
-      }
-    },
-  });
-
-  if (activeRounds._count.id > 0) {
-    return NextResponse.json({ code: "BAD_REQUEST", message: "There are active rounds" });
-  }
-
-  const round = await prisma.visor_Round.update({
-    where: { id },
-    data: {
-      status: "activa",
-    }
-  });
-
-  return round;
-
-}
-
-async function pauseRound(id: string) {
-  const round = await prisma.visor_Round.update({
-    where: { id },
-    data: {
-      status: "pausada",
-    }
-  });
-
-  return round;
-}
-
-async function stopRound(id: string) {
-  const round = await prisma.visor_Round.update({
-    where: { id },
-    data: {
-      status: "terminada",
-    }
-  });
-
-  return round;
 }
---
+++ src/app/dashboard/visor/teams/[id]/rounds/page.tsx
@@ -0,0 +1,157 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { Button, Divider } from "@nextui-org/react";
+import RoundsCard from "@/components/visor/teams/RoundsCard";
+import { useParams } from "next/navigation";
+
+interface Round {
+  id: string;
+  name: string;
+  status: "activa" | "pausada" | "noiniciada";
+  pointTypesIDs: string[];
+}
+
+interface Rounds {
+  active: Round[]
+  paused: Round[]
+  noStarted: Round[]
+}
+
+export default function RoundsPage() {
+  const { id: teamId } = useParams();
+  const [alertMessage, setAlertMessage] = useState<string | null>(null);
+  const [rounds, setRounds] = useState<Rounds>({
+    active: [],
+    noStarted: [],
+    paused: []
+  });
+
+  const getRounds = async () => {
+    try {
+      const resBody = await fetch(`/dashboard/api/visor/teams/${teamId}/rounds`)
+        .then(res => res.json())
+        .catch(e => console.log(e));
+
+      if (resBody.code !== "OK") {
+        console.error(resBody.message);
+        alert("Error Obteniendo las Rondas");
+      }
+
+      console.log(resBody.data);
+
+      setRounds(resBody.data);
+    } catch (error) {
+      console.error("Error fetching rounds:", error);
+    }
+  };
+
+  useEffect(() => {
+    getRounds();
+  }, []);
+
+  // useEffect(() => {
+  //   console.log("Rounds state updated:", rounds);
+  //   const activeRounds = rounds.filter((round) => round.status === "activa");
+  //   const pausedRounds = rounds.filter((round) => round.status === "pausada");
+
+  //   if (activeRounds.length > 1) {
+  //     setAlertMessage("Sólo puede haber una ronda activa. Ajustando las rondas...");
+  //     const adjustedRounds = rounds.map((round, index) => {
+  //       if (round.status === "activa" && index > 0) {
+  //         return { ...round, status: "noiniciada" };
+  //       }
+  //       return round;
+  //     });
+  //     setRounds(adjustedRounds as Round[]);
+  //   }
+
+  //   if (pausedRounds.length > 1) {
+  //     setAlertMessage("Sólo puede haber una ronda pausada. Ajustando las rondas...");
+  //     const adjustedRounds = rounds.map((round, index) => {
+  //       if (round.status === "pausada" && index > 0) {
+  //         return { ...round, status: "noiniciada" };
+  //       }
+  //       return round;
+  //     });
+  //     setRounds(adjustedRounds as Round[]);
+  //   }
+  // }, [rounds]);
+
+  const handleStatusChange = () => {
+    getRounds();
+  };
+
+  return (
+    <div className="flex flex-col p-8 gap-8">
+      {alertMessage && (
+        <div className="alert alert-warning flex justify-between items-center p-4 mb-4 text-sm text-yellow-700 bg-yellow-100 rounded-lg" role="alert">
+          <span>{alertMessage}</span>
+          <Button isIconOnly className="bg-transparent" aria-label="Cerrar alerta" onPress={() => setAlertMessage(null)}>
+            <span className="material-symbols-outlined">close</span>
+          </Button>
+        </div>
+      )}
+
+      {rounds.active.length > 0 && (
+        <div className="flex flex-col gap-4 w-full">
+          <h1 className="text-3xl">Ronda Activa</h1>
+          <div className="w-full">
+            {rounds.active.map((activeRound) =>
+              <RoundsCard
+                key={activeRound.id}
+                id={activeRound.id}
+                name={activeRound.name}
+                status={activeRound.status}
+                pointTypesIDs={activeRound.pointTypesIDs}
+                onStatusChange={handleStatusChange}
+              />
+            )}
+          </div>
+          <Divider />
+        </div>
+      )}
+
+      {rounds.paused.length > 0 && (
+        <div className="flex flex-col gap-4 w-full">
+          <h1 className="text-3xl">Ronda Pausada</h1>
+          <div>
+            {rounds.paused.map((pausedRound) =>
+              <RoundsCard
+                key={pausedRound.id}
+                id={pausedRound.id}
+                name={pausedRound.name}
+                status={pausedRound.status}
+                pointTypesIDs={pausedRound.pointTypesIDs}
+                onStatusChange={handleStatusChange}
+              />
+            )}
+          </div>
+          <Divider />
+        </div>
+      )}
+
+      {rounds.noStarted.length > 0 &&
+        <div className="flex flex-col gap-4 w-full">
+          <div className="flex flex-row justify-between">
+            <h1 className="text-3xl">Futuras Rondas</h1>
+            <Button color="primary">Añadir nueva ronda</Button>
+          </div>
+          <div className="flex flex-col gap-4">
+            {rounds.noStarted.map((round) => (
+              <RoundsCard
+                key={round.id}
+                id={round.id}
+                name={round.name}
+                status={round.status}
+                pointTypesIDs={round.pointTypesIDs}
+                onStatusChange={handleStatusChange}
+              />
+            ))}
+          </div>
+        </div>
+      }
+
+    </div>
+  );
+}
