a6ce6982323dc44b45d848b60b96a492b75f809f
Diego Martinez <diegogama8934@gmail.com>
2024-07-18 03:24:44 -0600

    add: map draw polygones when zoom change

---
+++ src/app/dashboard/visor/map/page.tsx
@@ -0,0 +1,49 @@
+"use client";
+import { useState } from "react";
+import Map from "@/components/visor/maps/Map";
+import Marker from "@/components/visor/maps/Marker";
+import PopUp from "@/components/visor/maps/PopUp";
+
+interface Point {
+  lat: number
+  lng: number
+}
+
+export default function GeneralMapPage() {
+  const [points, setPoints] = useState<Point[]>([
+    { lat: 20, lng: -100 },
+    { lat: 21, lng: -101 },
+    { lat: 22, lng: -102 },
+    { lat: 23, lng: -103 }
+  ]);
+
+  return (
+    <div className="flex flex-1 gap-8 p-8 h-full">
+      <Map showPolygones className="flex flex-1 h-full">
+        {
+          points.map((point, index) => (
+            <Marker key={index} position={point}>
+              <PopUp>
+                <h3>Point {index}</h3>
+
+                <p>Description</p>
+              </PopUp>
+            </Marker>
+          ))
+        }
+      </Map>
+
+      <div className="flex flex-col">
+
+        {/* Componente de Alan, me imagino que va a poder setear el state de points de acuerdo a los filtros */}
+
+        <h2 className="text-center">Filtros</h2>
+
+        <div className="flex-1 w-72 bg-zinc-200">
+          Aqui van los filtros de alan
+        </div>
+
+      </div>
+    </div>
+  );
+}
--- src/components/visor/maps/Map.tsx
+++ src/components/visor/maps/Map.tsx
@@ -6,6 +6,31 @@ import { createContext, useEffect, useRef, useState } from "react";
 const defaultMapCenter = { lat: 20.84651181570421, lng: -99.79102603354762 };
 const defaultMapZoom = 9;
 
+const figures1Paths = [ // This could be electoral sections
+  { "lat": 37.7749, "lng": -122.4194 },
+  { "lat": 37.7849, "lng": -122.4094 },
+  { "lat": 37.7749, "lng": -122.3994 }
+];
+const figures2Paths = [ // This could be local district
+  { "lat": 37.7749, "lng": -122.4294 },
+  { "lat": 37.7749, "lng": -122.4194 },
+  { "lat": 37.7649, "lng": -122.4194 },
+  { "lat": 37.7649, "lng": -122.4294 }
+];
+const figures3Paths = [ // This could be federal district
+  { "lat": 37.7849, "lng": -122.4194 },
+  { "lat": 37.7949, "lng": -122.4094 },
+  { "lat": 37.7949, "lng": -122.3994 },
+  { "lat": 37.7849, "lng": -122.3894 },
+  { "lat": 37.7749, "lng": -122.3994 }
+];
+const polygonOptions = { // This must be the deseable style of the polygons
+  strokeColor: "#FF0000",
+  strokeOpacity: 0.8,
+  strokeWeight: 2,
+  fillColor: "#FF0000",
+  fillOpacity: 0.35
+};
 interface MapContext {
   loader: Loader | null;
   map: google.maps.Map | null;
@@ -16,6 +41,7 @@ interface Props {
   zoom?: number,
   className?: string
   onClick?: (e: google.maps.MapMouseEvent) => void
+  showPolygones: boolean
 }
 
 export const mapContext = createContext<MapContext>({
@@ -28,12 +54,17 @@ export default function Map({
   center = defaultMapCenter,
   zoom = defaultMapZoom,
   className,
-  onClick
+  onClick,
+  showPolygones
 }: Props) {
   const mapRef = useRef<HTMLDivElement>(null);
   const [loader, setLoader] = useState<Loader | null>(null);
   const [map, setMap] = useState<google.maps.Map | null>(null);
 
+  // !Tried to do this. Failed because loader had not been loaded yet
+  // const figures1 = new google.maps.Polygon({ paths: figures1Paths, ...polygonOptions });
+  // const figures2 = new google.maps.Polygon({ paths: figures2Paths, ...polygonOptions });
+  // const figures3 = new google.maps.Polygon({ paths: figures3Paths, ...polygonOptions });
   useEffect(() => {
     const initMap = async () => {
       const loader = new Loader({
@@ -60,10 +91,57 @@ export default function Map({
     initMap();
   }, [center, zoom]);
 
+  useEffect(() => {
+    if (!showPolygones) return;
+    if (!map) return;
+
+    const figures1 = new google.maps.Polygon({ paths: figures1Paths, ...polygonOptions });
+    const figures2 = new google.maps.Polygon({ paths: figures2Paths, ...polygonOptions });
+    const figures3 = new google.maps.Polygon({ paths: figures3Paths, ...polygonOptions });
+
+    const polygonsListener = map.addListener("zoom_changed", () => {
+      drawPolygones(figures1, figures2, figures3);
+    });
+
+    return () => {
+      console.log("Removing listeners...");
+      google.maps.event.removeListener(polygonsListener);
+      figures1.setMap(null);
+      figures2.setMap(null);
+      figures3.setMap(null);
+    };
+
+  }, [showPolygones, map]);
+
   const initListeners = (myMap: google.maps.Map | null) => {
     if (onClick) myMap?.addListener("click", (e: google.maps.MapMouseEvent) => onClick(e));
   };
 
+  const drawPolygones = (figures1: google.maps.Polygon, figures2: google.maps.Polygon, figures3: google.maps.Polygon) => {
+    const currentZoom = map!.getZoom();
+    console.log("drawPolygones");
+    console.log(currentZoom);
+
+    if (!currentZoom) return;
+
+    if (currentZoom >= 8 && currentZoom < 12) {
+      figures1.setMap(map);
+      figures2.setMap(null);
+      figures3.setMap(null);
+    }
+
+    if (currentZoom >= 12 && currentZoom < 15) {
+      figures1.setMap(null);
+      figures2.setMap(map);
+      figures3.setMap(null);
+    }
+
+    if (currentZoom >= 15 && currentZoom < 18) {
+      figures1.setMap(null);
+      figures2.setMap(null);
+      figures3.setMap(map);
+    }
+  };
   return (
     <>
       <div id="map" className={className} ref={mapRef} />
