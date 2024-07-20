bcfdeb9a943bff27b08c1ead11bcae1194a192f8
Diego Martinez <diegogama8934@gmail.com>
2024-07-19 18:32:09 -0600

    add: init layout of field map. Problem: previous layouts are showed too

---
+++ src/app/dashboard/visor/teams/[id]/fieldMap/layout.tsx
@@ -0,0 +1,13 @@
+import Map from "@/components/visor/maps/Map";
+
+export default function FieldMapLayout() {
+
+
+  return (
+    <div className="flex flex-col flex-1 w-full">
+      <Map className="flex flex-1 w-full">
+        XD
+      </Map>
+    </div>
+  );
+}
