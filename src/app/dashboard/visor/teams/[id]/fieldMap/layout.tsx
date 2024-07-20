de22b9d8e065d9fc6e97ff17898ef66932ba6a65
Diego Martinez <diegogama8934@gmail.com>
2024-07-19 21:52:46 -0600

    add: circle on user location, map re render each 3 seg, some problems in consecuence

--- src/app/dashboard/visor/teams/[id]/fieldMap/layout.tsx
+++ src/app/dashboard/visor/teams/[id]/fieldMap/layout.tsx
@@ -1,17 +1,25 @@
 "use client";
+import Circle from "@/components/visor/maps/Circle";
 import Map from "@/components/visor/maps/Map";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+interface Location {
+  lat: number
+  lng: number
+}
 
 export default function FieldMapLayout() {
+  const [userLocation, setUserLocation] = useState<Location | undefined>(undefined);
 
   useEffect(() => {
     const userLocationInterval = setInterval(getUserLocation, 3000);
 
     function getUserLocation() {
       navigator.geolocation.getCurrentPosition(
-        (position) => {
-          console.log("Position... ");
-          console.log(position.coords.latitude, position.coords.longitude);
+        ({ coords }) => {
+          const { latitude, longitude } = coords;
+          console.log("Position... ", latitude, longitude);
+          setUserLocation({ lat: latitude, lng: longitude });
         },
         (err) => console.log("Error: ", err),
         {
@@ -26,7 +34,7 @@ export default function FieldMapLayout() {
   }, []);
   /*
 
-  TRIED TO THIS
+  TRIED TO DO THIS
 
   The following should work as the previous, but it doesn´t
 
@@ -46,9 +54,25 @@ export default function FieldMapLayout() {
 
   return (
     <div className="flex flex-col flex-1 w-full">
-      <Map className="flex flex-1 w-full">
-        XD
-      </Map>
+      {
+
+        <Map className="flex flex-1 w-full" center={userLocation} zoom={18}>
+          {
+            userLocation ? (
+              <Circle
+                center={userLocation}
+                radius={10}
+                fillColor="#4592D8"
+                fillOpacity={0.8}
+                strokeWeight={4}
+                strokeColor="#E2E2E2"
+              />
+            ) : (
+              <></>
+            )
+          }
+        </Map>
+      }
     </div>
   );
 }
