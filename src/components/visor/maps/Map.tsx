80ab688f29a6f6b7d3d8231071e23cfb56ed346c
Diego Martinez <diegogama8934@gmail.com>
2024-07-21 03:04:52 -0600

    change: map component dont create map on each render, still having 2 issues

--- src/components/visor/maps/Map.tsx
+++ src/components/visor/maps/Map.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { GCP_API_KEY } from "@/configs";
 import { Loader } from "@googlemaps/js-api-loader";
-import { createContext, useEffect, useRef, useState } from "react";
+import { createContext, useCallback, useEffect, useRef, useState } from "react";
 
 const defaultMapCenter = { lat: 20.84651181570421, lng: -99.79102603354762 };
 const defaultMapZoom = 9;
@@ -27,6 +27,7 @@ export default function Map({
   children,
   center = defaultMapCenter,
   zoom = defaultMapZoom,
+
   className,
   onClick
 }: Props) {
@@ -34,31 +35,57 @@ export default function Map({
   const [loader, setLoader] = useState<Loader | null>(null);
   const [map, setMap] = useState<google.maps.Map | null>(null);
 
+  const initMap = async () => {
+    const loader = new Loader({
+      apiKey: GCP_API_KEY,
+      version: "weekly",
+    });
+
+    const { Map } = await loader.importLibrary("maps");
+
+    const myMap = new Map(mapRef.current as HTMLElement, {
+      center: center,
+      zoom: zoom,
+      mapId: "mainMap",
+      clickableIcons: false,
+      mapTypeControl: false,
+      streetViewControl: false
+    });
+
+    initListeners(myMap);
+    setLoader(loader);
+    setMap(myMap);
+  };
+
   useEffect(() => {
-    const initMap = async () => {
-      const loader = new Loader({
-        apiKey: GCP_API_KEY,
-        version: "weekly",
-      });
-
-      const { Map } = await loader.importLibrary("maps");
-
-      const myMap = new Map(mapRef.current as HTMLElement, {
-        center: center,
-        zoom: zoom,
-        mapId: "mainMap",
-        clickableIcons: false,
-        mapTypeControl: false,
-        streetViewControl: false
-      });
-
-      initListeners(myMap);
-      setLoader(loader);
-      setMap(myMap);
-    };
-
     initMap();
-  }, [center, zoom]);
+  }, []);
+
+  useEffect(() => {
+    if (!map) return;
+
+    map?.setCenter(center);
+    map?.setZoom(zoom);
+  }, [center, zoom, map]);
+
+  /*
+    With this code, each 3 seconds, the map focus on the current user position
+    and the zoom passed in the props.
+
+    I think we can solve this doing:
+
+    - A map control that makes map focusing on the zoom level and user position.
+      (both passed by the props)
+    - To do the point above, we need to create another prop to know if the map
+      is being used in field or in office.
+      So... Map.isFieldMap
+        TRUE: then show the control to focus user position.
+        FALSE: work as it has been working
+    
+    I also think this will make the component less maintainable and scalable.
+    So for now I think I will create another component named FieldMap that will
+    only be called if we need to use a field map
+  */
 
   const initListeners = (myMap: google.maps.Map | null) => {
     if (onClick) myMap?.addListener("click", (e: google.maps.MapMouseEvent) => onClick(e));
