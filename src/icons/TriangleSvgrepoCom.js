0014c3bbf9252e0da45c020ddbb4e1d1a5eb035b
Costas Rueda Juan Pablo <jcostas29@alumnos.uaq.mx>
2024-07-22 01:18:36 -0600

    Component creator out of SVG file

---
+++ src/icons/TriangleSvgrepoCom.js
@@ -0,0 +1,19 @@
+import * as React from "react";
+const SvgTriangleSvgrepoCom = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      fill="#758CA3"
+      fillRule="evenodd"
+      d="m13.789 1.578 9.764 19.528A2 2 0 0 1 21.763 24H2.237a2 2 0 0 1-1.789-2.894l9.764-19.528a2 2 0 0 1 3.578 0"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+export default SvgTriangleSvgrepoCom;
